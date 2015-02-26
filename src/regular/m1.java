d763931763b6889e190b99eb586f0566b459f1d4
ten <muyuesanter@gmail.com>
2015-02-26 16:33:04 -0500

    3

--- src/regular/m1.java
+++ src/regular/m1.java
@@ -1,23 +1,32 @@
 package regular;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 public class m1 extends JApplet {
  public void init(){
-	 this.setLayout(new BorderLayout());
+	 this.setLayout(new FlowLayout());
+	 
 	 JTextField tf = new JTextField(10);
-	 add(tf,BorderLayout.NORTH);
+	// add(tf,BorderLayout.NORTH);
+	 add(tf);
 	 
 	 JButton jb = new JButton("Clear");
-	 add(jb,BorderLayout.EAST);
+	// add(jb,BorderLayout.EAST);
+	 add(jb);
 	 
+	 //JScrollPane h = new JScrollPane(0,1),
+		//	 v = new JScrollPane(1,0);
+	
+	 //add((new m1Carvas(tf,jb)), BorderLayout.SOUTH);
+	 add((new m1Carvas(tf,jb)));
 	 
-	 add((new m1Carvas(tf,jb)), BorderLayout.SOUTH);
-	 
-	 setSize(600,600);
+	 setSize(1000,600);
  }
 }
--- src/regular/m1Carvas.java
+++ src/regular/m1Carvas.java
@@ -13,14 +13,15 @@ import javax.swing.*;
 public class m1Carvas extends Canvas implements ActionListener{
 	JTextField tf;
 	JButton clear;
-	
+	//JScrollPane h = new JScrollPane(0,1),
+		//		v = new JScrollPane(1,0);
 	int r = 1;
-	int numTh=1;
-	int x=250, y=250;
+	int numTh=0;
+	int x=10, y=50;
 	
 	public static Vector<Integer> fibArr = new Vector<Integer>(0);
-	public static Vector<Point2D> centerPoint = new Vector<Point2D>(0);
-	Point2D p;
+	public static Vector<Point2D> centerPoint = new Vector<Point2D>();
+	Point2D p =null;
 	
 	public m1Carvas(JTextField tf, JButton clear){
 		
@@ -29,31 +30,33 @@ public class m1Carvas extends Canvas implements ActionListener{
 		this.clear= clear;
 		clear.addActionListener(this);
 		
-		setSize(500,500);
+		setSize(1000,500);
 		setVisible(true);
 		setBackground(Color.black);
+		
 
 	}
 	
 	public void paint(Graphics g) {
 		g.setColor(Color.white);
-		for(int i=1 ; i< numTh ; i++){
-			r= fibo(i);
-			g.drawArc(x, y, r*20, r*20, 0+(i-1)*90, 90);
+		for(int i=0 ; i<numTh ; i++){
+			r= fibo(i+1);
+			//g.drawArc((int) cp(i+1).getX(),(int) cp(i+1).getY(), r*20, r*20, 0+(i)*90, 90);
+			g.drawArc(x,y, r*20, r*20, 0+(i)*90, 90);
+		}
+		
 		g.setColor(Color.white);
-		for(int j = 0; j< numTh ; j++)
-			g.drawString(Integer.toString(fibo(j+1)), 50+j*10, 50);
-			
-		}	
+		for(int j = 0; j< numTh ; j++){
+			x+=100;
+			if(j%10 == 0){
+				y+=20;
+				x=10;
+			}
+			g.drawString(Integer.toString(fibo(j+1)), x, y);
+		}
 	}
 
 	public int fibo(int numTh) {
-	    /*
-		if(numTh<=2)
-			return 1;
-		else
-			return fibo(numTh-1) + fibo(numTh-2);
-//		*/
 		if(fibArr.size() == 0){
 			fibArr.add(1);
 			fibArr.add(1);
@@ -63,31 +66,55 @@ public class m1Carvas extends Canvas implements ActionListener{
 				fibArr.add(fibArr.lastElement() + fibArr.elementAt(fibArr.size()-2) );
 			}
 		return fibArr.elementAt(numTh-1);
-		
-		
 	}
 	
-	public Point2D CP(int numTh){
+	public Point2D cp(int numTh){
 		int offsetOfCP = 0;
-		int offsetDirect = 0; // 1: right 2: up
+		int offsetDirect = 4; // 0:down, 1: right, 2: up, 3:left, 
+		
 		if(centerPoint.size() == 0){
-			p.setLocation(250,250);
-			p.setLocation(250,250);	
+			p.setLocation(200.0,200.0);
+			centerPoint.add(p);
+			centerPoint.add(p);
 		}
-		else{
-			if(numTh > centerPoint.size()){
-				offsetDirect = (numTh -2)%4 ;
-				centerPoint.add();
+		else if(numTh > centerPoint.size())
+			for(int k = centerPoint.size(); k < numTh; k++){
+				offsetOfCP = fibo(k);
+				offsetDirect = (k - 1) % 4;
+				switch(offsetDirect){
+				case 0:{
+					p.setLocation(cp(k).getX(), cp(k).getY()+offsetOfCP);
+					centerPoint.add(p);
+					break;
+				}
+				case 1:{
+					p.setLocation(cp(k).getX()+offsetOfCP, cp(k).getY());
+					centerPoint.add(p);
+					break;
+				}
+				case 2:{
+					p.setLocation(cp(k).getX(), cp(k).getY()-offsetOfCP);
+					centerPoint.add(p);
+					break;
+				}
+				case 3:{
+					p.setLocation(cp(k).getX()-offsetOfCP, cp(k).getY());
+					centerPoint.add(p);
+					break;
+				}
+				}
+				
 			}
-		}
-		return null;
+		
+		return centerPoint.elementAt(numTh-1);
 	}
 	
 	@Override
  	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == tf){
 			numTh = Integer.valueOf(tf.getText()) ;
-			
+			for(int i = 0 ; i< numTh;i++)
+				System.out.println(cp(i));
 			
 		}
 		else numTh =0;
